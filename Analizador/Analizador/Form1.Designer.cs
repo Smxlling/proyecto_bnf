d4ae5cd9c117fa001095809a950a54af72157ec0
Smxlling <44947856+Smxlling@users.noreply.github.com>
2019-10-08 07:43:17 -0600

    Add files via upload

---
+++ Analizador/Analizador/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Analizador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lexico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(61, 309);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalizar.TabIndex = 0;
+            this.btnAnalizar.Text = "analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Location = new System.Drawing.Point(32, 50);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(354, 218);
+            this.txtEditor.TabIndex = 1;
+            this.txtEditor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.AllowUserToAddRows = false;
+            this.dgvInfo.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.token,
+            this.lexico});
+            this.dgvInfo.GridColor = System.Drawing.SystemColors.Window;
+            this.dgvInfo.Location = new System.Drawing.Point(474, 50);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.Size = new System.Drawing.Size(239, 218);
+            this.dgvInfo.TabIndex = 2;
+            // 
+            // token
+            // 
+            this.token.HeaderText = "Token";
+            this.token.Name = "token";
+            // 
+            // lexico
+            // 
+            this.lexico.HeaderText = "Lexico";
+            this.lexico.Name = "lexico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 380);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.btnAnalizar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lexico;
+    }
+}
+
---
+++ Analizador/Analizador/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador
+{
+    public partial class Form1 : Form
+    {
+        List<string> reservadas = new List<string>();
+
+        string cadena = " ";
+        int tam = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reservadas.Add("int");
+            reservadas.Add("string");
+            reservadas.Add("public");
+            reservadas.Add("if");
+            reservadas.Add("}");
+            reservadas.Add("{");
+            reservadas.Add("(");
+            reservadas.Add(")");
+            reservadas.Add("while");
+        }
+
+        public Boolean palabrasReservadas(String palabra)
+
+        {
+
+
+
+
+            for (int i = 0; i < reservadas.Count(); i++)
+            {
+                if (palabra == reservadas[i]) {
+
+
+
+                    dgvInfo.Rows.Add("reservada", palabra);
+
+                }
+            }
+
+
+
+
+            return true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Boolean tokenFinalizado = false;
+            int t = 0;
+            char[] arreglo = txtEditor.Text.ToCharArray();
+            string[] cadena = new string[t];
+            List<String> sinta = new List<string>();
+
+
+            string palabra = "";
+            dgvInfo.Rows.Clear();
+            dgvInfo.Refresh();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == ' ')
+                {
+                    palabrasReservadas(palabra);
+
+                    sinta.Add(palabra);
+
+                    palabra = "";
+                }
+                else if (arreglo[i] == '(' || arreglo[i] == ')' || arreglo[i] == '}' || arreglo[i] == '{')
+                {
+                    sinta.Add(arreglo[i].ToString());
+                }
+                else
+                {
+                    palabra += arreglo[i];
+
+                }
+
+                
+            }
+            if (arreglo[arreglo.Length-1]!=';') {
+                MessageBox.Show("Token no finalizado");
+            }
+
+            sinta.Add(palabra);
+            palabrasReservadas(palabra);
+            for(int i=0;i < arreglo.Length;i++)
+            {
+              
+            }
+            
+
+        }
+
+       
+
+
+    }
+}
